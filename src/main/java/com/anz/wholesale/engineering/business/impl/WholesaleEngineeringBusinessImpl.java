9cedf7344204606be633b136e4a26a700b1bc845
vijaykrishnagv <32089242+vijaykrishnagv@users.noreply.github.com>
2020-11-20 11:25:08 +0530

    Add files via upload

--- src/main/java/com/anz/wholesale/engineering/business/impl/WholesaleEngineeringBusinessImpl.java
+++ src/main/java/com/anz/wholesale/engineering/business/impl/WholesaleEngineeringBusinessImpl.java
@@ -3,12 +3,16 @@ package com.anz.wholesale.engineering.business.impl;
 import com.anz.wholesale.engineering.business.WholesaleEngineeringBusiness;
 import com.anz.wholesale.engineering.respository.AccountsRepository;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WholesaleEngineeringBusinessImpl implements WholesaleEngineeringBusiness {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(WholesaleEngineeringBusiness.class);
 
   @Autowired
   private AccountsRepository accountsRepository;
--- src/main/java/com/anz/wholesale/engineering/controller/WholesaleEngineeringController.java
+++ src/main/java/com/anz/wholesale/engineering/controller/WholesaleEngineeringController.java
@@ -2,6 +2,8 @@ package com.anz.wholesale.engineering.controller;
 
 import com.anz.wholesale.engineering.business.impl.WholesaleEngineeringBusinessImpl;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/anz-wholesale-engineering")
 public class WholesaleEngineeringController {
-
+  private static final Logger  LOGGER = LoggerFactory.getLogger(WholesaleEngineeringController.class);
   @Autowired
   private WholesaleEngineeringBusinessImpl wholesaleEngineeringBusiness;
 
@@ -26,7 +28,10 @@ public class WholesaleEngineeringController {
    */
   @GetMapping(value = "/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<String> fetchAccounts() {
+
+    LOGGER.trace("Received request to fetch User accounts");
     List<Object> accountsList = wholesaleEngineeringBusiness.fetchAccounts();
+    LOGGER.debug("No. of accounts found : {}",accountsList.size());
     return new ResponseEntity<String>(accountsList.toString(), HttpStatus.OK);
   }
 
@@ -40,8 +45,11 @@ public class WholesaleEngineeringController {
   public ResponseEntity<Object> fetchTransactions(
       @RequestParam(name = "account_number", required = true) String accountNumber,
       @RequestParam(name = "account_type", required = true) String accountType) {
+
+    LOGGER.info("Received request to fetch transaction details of an user account {} and account type : {}",accountNumber, accountType);
     List<Object> transactionList = wholesaleEngineeringBusiness
         .fetchTransactions(accountNumber, accountType);
+    LOGGER.debug("No. of transactions found : {} for account number: {}",transactionList.size(), accountNumber);
     return new ResponseEntity<>(transactionList, HttpStatus.OK);
   }
 
